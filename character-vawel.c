e356d09135b9e0eb832cfd16c282df7f90ed5b85
Irfan-Ali-1 <irfan0ali111@gmail.com>
2026-09-05 22:12:23 +0530

    calculater

---
+++ character-vawel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>      
+
+
+int main()
+{
+    char ch;
+    printf("enter a character:");
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        printf("the character is a vowel");
+    }
+    else
+    {
+        printf("the character is not a vowel");
+    }
+    return 0;
+}
